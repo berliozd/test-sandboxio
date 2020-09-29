0fe8497117a323817441f665eeb9406e0331dd34
Didier Berlioz <berliozd@gmail.com>
2020-09-29 09:46:15 +0200

    adding a simple react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles.css";
+import { Welcome } from "./comp";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
+      <Welcome />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
